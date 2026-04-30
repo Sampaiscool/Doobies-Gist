1f1f419bfe7cd992d2f1681e13b8598f068423eb
semvandernat <semvandernat@gmail.com>
2026-04-30 15:07:47 +0200

    The great removal of the sudden extra directory

--- Doobies-Gist-features/Assets/Scripts/Classes/UsedOnce/LocationEffectSO.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class LocationEffectSO : ScriptableObject, ILocationEffect
-{
-    public abstract void ApplyEffect();
-}
